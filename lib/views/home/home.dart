574db967f6666ef4c03c54971cc687fed13f0fab
TimingAttck <ohlhoffclaude@hotmail.com>
2021-10-29 23:02:41 +0200

    Added a time range slection to the activity selection.

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bottom_sheet/bottom_sheet.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +8,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_lighting/bloc/preferences/preferences_bloc.dart';
 import 'package:smart_lighting/themes/themes.dart';
 import 'package:smart_lighting/views/details/details.dart';
+import 'package:smart_lighting/widgets/general/status_button.dart';
+import 'package:time_range/time_range.dart';
 
 class Home extends StatefulWidget {
 
@@ -15,6 +19,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  StateButtonController stateButtonController = StateButtonController();
 
   var items = [
     Activity(
@@ -77,7 +83,10 @@ class _HomeState extends State<Home> {
           }, icon: Icon(iconData) )
         ],
       ),
-      body: Padding(padding: const EdgeInsets.fromLTRB(15, 20, 15, 0), child: buildGridView(buildCards(items, theme == ThemeHelper.LightTheme ? "light" : "dark"))),
+      body: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+          child: buildGridView(buildCards(items, theme))
+      ),
     );
 
     SystemChrome.setSystemUIOverlayStyle(theme.systemOverlay);
@@ -95,8 +104,9 @@ class _HomeState extends State<Home> {
       padding: const EdgeInsets.only(bottom: 20),
   );
 
-  List<Widget> buildCards(List<Activity> items, themeName) {
+  List<Widget> buildCards(List<Activity> items, ThemeWrapper themeWrapper) {
 
+    String themeName = themeWrapper == ThemeHelper.LightTheme ? "light" : "dark";
     List<Widget> cards = [];
 
     for (var item in items) {
@@ -104,11 +114,11 @@ class _HomeState extends State<Home> {
           onTap: () {
             showFlexibleBottomSheet(
               minHeight: 0,
-              initHeight: 0.5,
+              initHeight: 0.63,
               maxHeight: 1,
               context: context,
               builder: (BuildContext context, ScrollController scrollController, double offset) {
-                  return _buildBottomSheet(context, scrollController, offset, item, themeName);
+                  return _buildBottomSheet(context, stateButtonController, scrollController, offset, item, themeWrapper);
               },
               anchors: [0, 0.5, 1],
             );
@@ -160,13 +170,46 @@ class Activity {
 
 }
 
+StateButton _buildSetActivityButton(BuildContext context, ThemeWrapper theme, StateButtonController stateButtonController) {
 
+  return StateButton(
+    disabled: true,
+    alignment: Alignment.bottomRight,
+    height: 40,
+    child: const Text(
+      "Set Activity",
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      textAlign: TextAlign.center,
+      textScaleFactor: 1.0,
+    ),
+    controller: stateButtonController,
+    onPressed: () {
+      stateButtonController.start();
+      Timer(const Duration(seconds: 3), () {
+        stateButtonController.success();
+        Timer(const Duration(seconds: 2), () {
+          Navigator.pop(context);
+        });
+      });
+    },
+    backgroundColor: theme.themeData.accentColor,
+    borderRadius: 8,
+  );
 
-Widget _buildBottomSheet(BuildContext context, ScrollController scrollController, double bottomSheetOffset, Activity activity, String themeName) {
+}
+
+Widget _buildBottomSheet(BuildContext context, StateButtonController stateButtonController, ScrollController scrollController, double bottomSheetOffset, Activity activity, ThemeWrapper themeWrapper) {
+
+  DateTime now = DateTime.now();
+
+  String themeName = themeWrapper == ThemeHelper.LightTheme ? "light" : "dark";
+  StateButton stateButton = _buildSetActivityButton(context, themeWrapper, stateButtonController);
 
   return SafeArea(
     child: Material(
-      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
       child: Padding(
         padding: const EdgeInsets.only(bottom: 10),
         child: Stack(
@@ -195,38 +238,69 @@ Widget _buildBottomSheet(BuildContext context, ScrollController scrollController
                 ),
               ),
               const SizedBox(
-                height: 150,
-                child: Expanded(
-                    child: SingleChildScrollView(
+                height: 5,
+              ),
+              const SizedBox(
+                height: 75,
+                child: SingleChildScrollView(
                       child: Padding(
                           padding: EdgeInsets.only(left: 15, right: 15),
                           child: Text(
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sodales lacus tellus, ac ornare mi tempus ut. Mauris et nunc condimentum, consequat massa vel, tristique nulla. Morbi sit amet egestas est. Integer tortor sapien, fringilla sit amet felis sit amet, imperdiet faucibus tellus. Praesent sodales, tortor non interdum dictum, mi eros varius nisi, vel suscipit dolor tortor vel turpis. Suspendisse sollicitudin congue arcu, sit amet interdum velit fringilla id. Suspendisse sit amet mauris eu lectus vestibulum tempor.",
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sodales lacus tellus, ac ornare mi tempus ut. Integer sodales lacus tellus, ac ornare mi tempus ut.",
+                            overflow: TextOverflow.ellipsis,
                             style: TextStyle(
                               fontSize: 18,
                             ),
+                            maxLines: 3,
                           )
                       ),
-                    )
                 ),
               ),
               const SizedBox(
-                height: 10,
+                height: 15,
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  child: TimeRange(
+                    fromTitle: const Text(
+                      'FROM',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    toTitle: const Text(
+                      'TO',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    borderColor: themeWrapper.timeSelectBorder,
+                    activeBorderColor: themeWrapper.activetimeSelectBorder,
+                    activeBackgroundColor: themeWrapper.activetimeSelectBackground,
+                    backgroundColor: Colors.transparent,
+                    activeTextStyle: themeWrapper.activeTimeSelectTextStyle,
+                    firstTime: TimeOfDay(hour: now.hour, minute: 00),
+                    lastTime: const TimeOfDay(hour: 24, minute: 00),
+                    timeBlock: 20,
+                    timeStep: 20,
+                    onRangeCompleted: (range) => {
+                      stateButton.controller.enable()
+                    },
+                  )
+              ),
+              const SizedBox(
+                height: 20,
               ),
               Container(
                   alignment: Alignment.bottomRight,
                   padding: const EdgeInsets.only(right: 20),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15))
-                    )
-                  ),
-                  onPressed: () => {
-
-                  },
-                  child: const Text("Set as Activity")
-              ))
+                  child: SizedBox(
+                      width: 110,
+                      child: stateButton
+                  )
+              )
             ]),
       ]))),
     );
